6eea661e20fe00787650f1809ecf8aa1a6072f2c
StevenPapasot <75527414+StevePapasot@users.noreply.github.com>
2024-02-27 23:46:15 +0200

    Fibonacci Solution

--- FRONT-END DEVELOPMENT/INTERMEDIATE JAVASCRIPT/index.js
+++ FRONT-END DEVELOPMENT/INTERMEDIATE JAVASCRIPT/index.js
@@ -129,3 +129,34 @@ while (numberOfBottles >= 0) {
 	numberOfBottles--;
     console.log(numberOfBottles + " " + bottleWord + " of beer on the wall.");
 }
+
+//Fibonacci Exercise
+function fibonacciGenerator (n) {
+    //Do NOT change any of the code above 👆
+        
+        //Write your code here:
+        var output = [];
+        if (n===1){
+            output = [0];
+        }
+        else if (n===2){
+            output = [0,1];
+        }
+        else {
+            output = [0,1];
+            
+            for (var i=2; i<n; i++){
+                output.push(output[output.length-2]+output[output.length-1]);
+            }
+        }
+        
+        
+        
+        
+        
+        //Return an array of fibonacci numbers starting from 0.
+        return output;
+    //Do NOT change any of the code below 👇
+    }
+    
+    
