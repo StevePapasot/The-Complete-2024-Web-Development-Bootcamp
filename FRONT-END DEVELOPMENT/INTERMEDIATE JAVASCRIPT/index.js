57d5b4f2be6bcb3b1f376f6ea6a9bdcbbf95aecd
StevenPapasot <75527414+StevePapasot@users.noreply.github.com>
2024-02-27 14:27:56 +0200

    Random number, if-else statement, comparators

--- FRONT-END DEVELOPMENT/INTERMEDIATE JAVASCRIPT/index.js
+++ FRONT-END DEVELOPMENT/INTERMEDIATE JAVASCRIPT/index.js
@@ -1,1 +1,34 @@
-// Random Love Generator
+// Random Love Calculator
+var myName = prompt("What is your name?");
+var myCrush = prompt("What is your crush's name?");
+var calcPers = Math.round((Math.random() * 100) + 1);
+alert("Hello " + myName + " , the  calculator shows that you are " + calcPers + "% in love with " + myCrush);
+console.log("Hello " + myName + " , the  calculator shows that you are " + calcPers + "% in love with " + myCrush);
+
+//Add if-else statement on the above exercise
+var myName = prompt("What is your name?");
+var myCrush = prompt("What is your crush's name?");
+var calcPers = Math.round((Math.random() * 100) + 1);
+alert("Hello " + myName + " , the  calculator shows that you are " + calcPers + "% in love with " + myCrush);
+console.log("Hello " + myName + " , the  calculator shows that you are " + calcPers + "% in love with " + myCrush);
+
+if (calcPers > 70){
+    alert("Your love score is " + calcPers + ". You love each other very much!");
+}else{
+    alert("Your love score is " + calcPers);
+}
+
+//Combining Comparators
+var myName = prompt("What is your name?");
+var myCrush = prompt("What is your crush's name?");
+var calcPers = Math.round((Math.random() * 100) + 1);
+alert("Hello " + myName + " , the  calculator shows that you are " + calcPers + "% in love with " + myCrush);
+console.log("Hello " + myName + " , the  calculator shows that you are " + calcPers + "% in love with " + myCrush);
+
+if (calcPers > 70){
+    alert("Your love score is " + calcPers + ". You love each other very much!");
+}else if (calcPers >30 && calcPers <= 70) {
+    alert("Your love score is " + calcPers + "%");
+}else{
+    alert("Your love score is " + calcPers + "%. That's not very good at all!");
+}
