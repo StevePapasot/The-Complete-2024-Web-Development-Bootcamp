08bb9ddb6fd3743e38b95f1f6d37b36bd55746e2
StevenPapasot <75527414+StevePapasot@users.noreply.github.com>
2024-02-28 19:45:14 +0200

    Drum button sound

--- FRONT-END DEVELOPMENT/ADVANCED JAVASCRIPT DOM MANIPULATION/Drum+Kit+Starting+Files/Drum Kit Starting Files/index.js
+++ FRONT-END DEVELOPMENT/ADVANCED JAVASCRIPT DOM MANIPULATION/Drum+Kit+Starting+Files/Drum Kit Starting Files/index.js
@@ -1,6 +1,40 @@
 var numberOfDrumButtons = document.querySelectorAll(".drum").length;
 for(var i=0; i<numberOfDrumButtons; i++){
-document.querySelector("button").addEventListener("click", function handleClick(){
-    alert("I got clicked!");
+document.querySelectorAll(".drum")[i].addEventListener("click", function handleClick(){
+   var buttonInnerHTML = this.innerHTML;
+    switch (buttonInnerHTML){
+        case "w":
+            var crash = new Audio('sounds/crash.mp3');
+            audio.play();
+        break;
+        case "a":
+            var kick = new Audio('sounds/kick.mp3');
+            audio.play();
+        break;
+        case "s":
+            var snare= new Audio('sounds/snare.mp3');
+            audio.play();
+        break;
+        case "d":
+            var tom1= new Audio('sounds/tom1.mp3');
+            audio.play();
+        break;
+        case "j":
+            var tom2= new Audio('sounds/tom2.mp3');
+            audio.play();
+        break;
+        case "k":
+            var tom3= new Audio('sounds/tom3.mp3');
+            audio.play();
+        break;
+        case "l":
+            var tom4= new Audio('sounds/tom4.mp3');
+            audio.play();
+        break;
+
+        default:
+    }
 });
 }
+
+
