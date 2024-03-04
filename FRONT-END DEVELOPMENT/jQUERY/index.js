a5fa62e5c2787cf51301bdce5881225237b520e4
StevenPapasot <75527414+StevePapasot@users.noreply.github.com>
2024-03-04 16:55:39 +0200

    Adding Event Listeners

--- FRONT-END DEVELOPMENT/jQUERY/index.js
+++ FRONT-END DEVELOPMENT/jQUERY/index.js
@@ -1,2 +1,19 @@
 //Manipulate Attributes
-$("a").attr("href","http://www.yahoo.com");
+// $("a").attr("href","http://www.yahoo.com");
+
+//Add Event Listener
+// $("h1").click(function(){
+//     $("h1").css("color","purple");
+// });
+
+// $("button").click(function(){
+//     $("h1").css("color","purple");
+// });
+
+// $("input").keypress(function(event){
+//     console.log(event.key);
+// });
+
+$("input").keypress(function(event){
+    $("h1").text(event.key);
+});
