58b53ede916d6a217901aed70dd2def4ae9bb055
StevenPapasot <75527414+StevePapasot@users.noreply.github.com>
2024-03-07 11:24:28 +0200

    Which Button is Pressed

--- FRONT-END DEVELOPMENT/BOSS LEVEL - THE SIMON GAME/Simon+Game+Challenge+Starting+Files/Simon Game Challenge Starting Files/index.js
+++ FRONT-END DEVELOPMENT/BOSS LEVEL - THE SIMON GAME/Simon+Game+Challenge+Starting+Files/Simon Game Challenge Starting Files/index.js
@@ -1,15 +1,21 @@
 let buttonColors = ["red","blue","green","yellow"];
 let gamePattern = [];
+let userClickedPattern = []; 
+
+$(".btn").click(function(){
+    let userChosenColor = $(this).attr("id");
+    userClickedPattern.push(userChosenColor);
+});
+
 function nextSequence(){
     let randomNumber = Math.floor(Math.random() * 4);
     let randomChosenColor = buttonColors[randomNumber];
+    gamePattern.push(randomChosenColor);
     console.log(randomChosenColor);
 
-    $("#randomChosenColor").animate({opacity: '1'},{duration: 2650});
+    $("#" + randomChosenColor).animate({opacity: '1'},{duration: 2650});
     let audio = new Audio("sounds/" + randomNumber + ".mp3");
     audio.play();
-
-
 }
 nextSequence()
 
