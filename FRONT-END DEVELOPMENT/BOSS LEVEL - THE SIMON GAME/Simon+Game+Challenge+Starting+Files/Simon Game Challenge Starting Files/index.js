d1f515e0550ed91045ba901d777ac070b9f4e170
StevenPapasot <75527414+StevePapasot@users.noreply.github.com>
2024-03-07 11:39:14 +0200

    Add Animation to User Clicks

--- FRONT-END DEVELOPMENT/BOSS LEVEL - THE SIMON GAME/Simon+Game+Challenge+Starting+Files/Simon Game Challenge Starting Files/index.js
+++ FRONT-END DEVELOPMENT/BOSS LEVEL - THE SIMON GAME/Simon+Game+Challenge+Starting+Files/Simon Game Challenge Starting Files/index.js
@@ -5,19 +5,29 @@ let userClickedPattern = [];
 $(".btn").click(function(){
     let userChosenColor = $(this).attr("id");
     userClickedPattern.push(userChosenColor);
+    playSound(userChosenColor);
+    animatePress(userChosenColor);
+    checkAnswer(userClickedPattern.length-1);
 });
 
 function nextSequence(){
     let randomNumber = Math.floor(Math.random() * 4);
     let randomChosenColor = buttonColors[randomNumber];
     gamePattern.push(randomChosenColor);
-    console.log(randomChosenColor);
 
     $("#" + randomChosenColor).animate({opacity: '1'},{duration: 2650});
+    playSound(randomChosenColor);
 }
 nextSequence()
 
 function playSound(name){
     let audio = new Audio("sounds/" + randomNumber + ".mp3");
     audio.play();
+}
+
+function animatePress(currentColor) {
+    $("#" + currentColor).addClass("pressed");
+    setTimeout(function () {
+      $("#" + currentColor).removeClass("pressed");
+    }, 100);
 }
