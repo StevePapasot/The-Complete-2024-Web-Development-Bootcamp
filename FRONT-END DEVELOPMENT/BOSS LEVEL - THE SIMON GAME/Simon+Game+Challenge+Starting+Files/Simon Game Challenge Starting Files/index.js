05ee3013ab6bbdf7db80cf22f5ff1850c7cca276
StevenPapasot <75527414+StevePapasot@users.noreply.github.com>
2024-03-07 11:14:32 +0200

    Step 3 - Button Animation and Sound

--- FRONT-END DEVELOPMENT/BOSS LEVEL - THE SIMON GAME/Simon+Game+Challenge+Starting+Files/Simon Game Challenge Starting Files/index.js
+++ FRONT-END DEVELOPMENT/BOSS LEVEL - THE SIMON GAME/Simon+Game+Challenge+Starting+Files/Simon Game Challenge Starting Files/index.js
@@ -4,5 +4,12 @@ function nextSequence(){
     let randomNumber = Math.floor(Math.random() * 4);
     let randomChosenColor = buttonColors[randomNumber];
     console.log(randomChosenColor);
+
+    $("#randomChosenColor").animate({opacity: '1'},{duration: 2650});
+    let audio = new Audio("sounds/" + randomNumber + ".mp3");
+    audio.play();
+
+
 }
 nextSequence()
+
