bf37c302652b55a0d9d69dae9dd9ea9a380fce7b
StevenPapasot <75527414+StevePapasot@users.noreply.github.com>
2024-03-29 16:36:59 +0200

    REPL and how to use node

---
+++ BACK-END DEVELOPMENT/nodejs/2.1+Using+Node/2.1 Using Node/index.js
@@ -0,0 +1,4 @@
+//Node Read Eval Print Loop (REPL)
+/*Is a computer environment where user inputs are read and evaluated, and then the results are returned
+to the user. We just type node in the terminal.*/
+console.log('Hello from Node!');
