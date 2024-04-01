6df5363b1439d2c745507e1e5344d12b15999880
StevenPapasot <75527414+StevePapasot@users.noreply.github.com>
2024-04-01 16:02:48 +0300

    Initialize npm and install Express

--- BACK-END DEVELOPMENT/Express js/3.1 Express Server/index.js
+++ BACK-END DEVELOPMENT/Express js/3.1 Express Server/index.js
@@ -1,3 +1,13 @@
+/*Create an Express Server:
+1. Create a directory
+2. Create index.js file.
+3. Initialize NPM.
+4. Install the Express package from npm.
+5. Write Server application in index.js
+6. Start server
+The official website of Express.js: https://expressjs.com/
+*/
+
 import express from "express";
 const app = express();
 const port = 3000;
