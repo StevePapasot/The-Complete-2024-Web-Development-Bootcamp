9a48f428ce8eb4c608d5d8163b1bd73671fba18a
StevenPapasot <75527414+StevePapasot@users.noreply.github.com>
2024-04-01 13:16:06 +0300

    npm completed

--- BACK-END DEVELOPMENT/nodejs/2.3+NPM/2.3 NPM/index.js
+++ BACK-END DEVELOPMENT/nodejs/2.3+NPM/2.3 NPM/index.js
@@ -1,2 +1,7 @@
 //npm is a location where we can find complete node codes. It is a community library. In there we can find tools and documentation that other people have created. https://www.npmjs.com/
 //The magic starts when we want to install an npm package and use it -> $ npm install <Something>
+
+var generateName = require('sillyname');
+var sillyName = generateName();
+
+console.log(`My name is ${sillyName}`);
