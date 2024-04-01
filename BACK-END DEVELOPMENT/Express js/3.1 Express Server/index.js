33538519ac4a2a8b2097cdd6094b0d14b7fb206e
StevenPapasot <75527414+StevePapasot@users.noreply.github.com>
2024-04-01 16:14:46 +0300

    Complete Express Start HTTP

--- BACK-END DEVELOPMENT/Express js/3.1 Express Server/index.js
+++ BACK-END DEVELOPMENT/Express js/3.1 Express Server/index.js
@@ -4,7 +4,7 @@
 3. Initialize NPM.
 4. Install the Express package from npm.
 5. Write Server application in index.js
-6. Start server
+6. Start server -> node yor_app js
 The official website of Express.js: https://expressjs.com/
 */
 
@@ -15,3 +15,9 @@ const port = 3000;
 app.listen(port, () => {
   console.log(`Server running on port ${port}.`);
 });
+
+
+/* We can see in which port our computer is currently listening for interactions from outside. Hit the following commands on the terminal.
+                  WINDOWS                               MAC/LINUX
+    netstat -ano | findstr "LISTENING"        sudo lsof -i -P -n | grep LISTEN
+*/
