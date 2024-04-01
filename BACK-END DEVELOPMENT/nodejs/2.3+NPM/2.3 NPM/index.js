5275d95dcc3d0804a5ffa7fb0b3add2ec9ca6890
StevenPapasot <75527414+StevePapasot@users.noreply.github.com>
2024-04-01 13:07:29 +0300

    NPM Install

--- BACK-END DEVELOPMENT/nodejs/2.3+NPM/2.3 NPM/index.js
+++ BACK-END DEVELOPMENT/nodejs/2.3+NPM/2.3 NPM/index.js
@@ -1,1 +1,2 @@
 //npm is a location where we can find complete node codes. It is a community library. In there we can find tools and documentation that other people have created. https://www.npmjs.com/
+//The magic starts when we want to install an npm package and use it -> $ npm install <Something>
