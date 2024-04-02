07861ddd2ead339e612aab1d03c377cbfde5b3fa
StevenPapasot <75527414+StevePapasot@users.noreply.github.com>
2024-04-02 13:45:15 +0300

    Middleware

--- BACK-END DEVELOPMENT/Express js/3.4 Middleware/index2.js
+++ BACK-END DEVELOPMENT/Express js/3.4 Middleware/index2.js
@@ -1,7 +1,10 @@
 import express from "express";
+import morgan from "morgan";
 
 const app = express();
 const port = 3000;
+
+app.use(morgan("combined"));
 
 app.get("/", (req, res) => {
   res.send("Hello");
