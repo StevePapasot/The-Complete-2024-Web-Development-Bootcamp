38c6544659fbb33ccc62709927dcc0c005087732
StevenPapasot <75527414+StevePapasot@users.noreply.github.com>
2024-07-19 18:23:46 +0300

    Connect our pgAdmin

--- BACK-END DEVELOPMENT/PostgreSQL/World+Capital+Quiz/index.js
+++ BACK-END DEVELOPMENT/PostgreSQL/World+Capital+Quiz/index.js
@@ -1,8 +1,19 @@
 import express from "express";
 import bodyParser from "body-parser";
+import pg from "pg";
 
 const app = express();
 const port = 3000;
+
+const db = new pg.Client({
+  user: "postgres",
+  host: "localhost",
+  database: "world",
+  password: "Opalakia13!",
+  port: 5432
+});
+
+db.connect();
 
 let quiz = [
   { country: "France", capital: "Paris" },
